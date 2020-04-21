15c413f792c7cde93884a54818a7b66316bc87e1
b9030342 <b9030342@my.shu.ac.uk>
2020-04-21 14:42:32 +0100

    .

--- cleanTest.R
+++ cleanTest.R
@@ -160,9 +160,11 @@ housing <- read_csv("raw data used/housing.csv")
 # rename some columns
 names(housing)[4] <- "Local authority"
 
-# filter out unwanted local authorities
+# filter out unwanted local authorities and the unwanted columns 
 y <- housing %>%
-  filter(`Local authority` %in% tst2)
+  filter(`Local authority` %in% la$`Local authority`)
+y <- y[3:100]
+view(y)
 
 # check counts
 counts <- table(y$`Local authority`, useNA ="ifany")
@@ -170,8 +172,8 @@ view(counts)
 
 # create a key(to be called'date') for the dates and a new column 
 # for the number of houses called 'new houses' as the dataset is unneccesarily wide
-houses <- gather (y, Date, 'new houses', -'Region/Country code', -'Region/Country name', -'Local authority code', -'Local authority')
-
+houses <- gather (y, Date, 'new houses', -'Local authority code', -'Local authority')
+view(houses)
 
 ############# checks ##################
 complete.cases(houses)
@@ -202,15 +204,14 @@ population <- read_csv("raw data used/Population 2.0.csv")
 
 # create new version to clean
 pop2 <- population
+view(pop2)
 
 # rename some columns
 names(pop2)[1] <- "Local authority"
 names(pop2)[2] <- "Local authority code"
 
-# change the order of the columns 
+# change the order of the columns then remove unnecessary columns 
 pop2 <- pop2[c(2,1,3,4,5,6,7,8,9,10)]
-
-# remove unnecessary columns 
 pop3 <- pop2[c(1,2,3,5,6,7,8,9,10)]
 
 # create a key(to be called'year') for the years and a new column 
@@ -222,7 +223,7 @@ pop4 <- gather (pop3, year, 'count', -'Local authority code', -'Local authority'
 pop4[,"month"] <- NA
 
 # change the order so month is next to the year 
-pop5 <- pop4[c(1,2,3,4,6,5)]
+pop5 <- pop4[c(1,2,3,6,4,5)]
 
 # change the data type to character
 pop5$month <- as.character(pop5$month)
