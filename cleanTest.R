fdffdad7f7a9bf13bd196e4370cf043a48d21729
b9030342 <b9030342@my.shu.ac.uk>
2020-04-24 17:05:37 +0100

    .

--- cleanTest.R
+++ cleanTest.R
@@ -262,23 +262,8 @@ write.csv(cleanpop, "clean/cleanpop.csv", row.names=F)
 
 ######################################################################################################################################################
 # Import Total live births by month and area of usual residence of Mother, 
-# England and Wales, September 2014 to August 2015
-live20142015 <- read_excel("raw data used/live births/livebirthsbymonthsexandladsept2014toaug2015ew.xls")
-
-# Import Live births by month and area of usual residence of Mother, 
-# England and Wales, September 2015 to August 2016
-
-
-# Import Live births by month and area of usual residence of Mother, 
-# England and Wales, September 2016 to August 2017
-
-
-# Import Live births by month and area of usual residence of Mother, 
-# England and Wales, September 2017 to August 2018
-
-
-# Import Live births by month and area of usual residence of Mother, 
-# England and Wales, September 2018 to August 2019
+# England and Wales, September 2014 to August 2019
+births1419 <- read_excel("raw data used/live births/Merged births.xlsx")
 
 
 ######################################################################################################################################################
