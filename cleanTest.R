23e9fb98c3afdc958d8acae66a252cf2cbd220ea
b9030342 <b9030342@my.shu.ac.uk>
2020-06-01 14:40:16 +0100

    .

--- cleanTest.R
+++ cleanTest.R
@@ -1,5 +1,6 @@
 # remove all variables to clear the workspace 
-# rm(list=ls())
+rm(list=ls())
+
 # Install packages (run lines individually to ensure smooth installation)
 # check for updates. If there are any for the below packages, update by clicking the refresh button
 install.packages(c("boot", "class", "foreign", "lattice", "MASS", "nlme", "nnet", "survival"))
@@ -8,7 +9,7 @@ install.packages("data.table")
 install.packages("sqldf")
 install.packages("anytime")
 
-
+# load packages
 library(plyr)
 library(dplyr)
 library(readxl)
@@ -19,12 +20,13 @@ library(data.table)
 library(sqldf)
 library(anytime)
 
-# filter data to only show the local authorities we are interested in (the 10 below make up greater manchester)
+# the 10 local authorities (and their codes) in the dataset below make up greater manchester
 la <- read_excel("raw data used/local authorities.xlsx")
 
 # Import the time IDs
 time <- read_excel("raw data used/DimTime v2.xlsx")
 
+# Import the provider IDs
 provider <- read_excel("raw data used/Provider Type.xlsx")
 
 
@@ -34,6 +36,7 @@ provider <- read_excel("raw data used/Provider Type.xlsx")
 # Import dataset, specify range and give appropriate name 
 childcare <- read_excel("raw data used/Education_Childcare_dataset_as_at_31_March_2018_new (version 1).xlsx",sheet = "Childcare_providers", col_types = c("text","text", "text", "text", "date", "text","text", "text", "text", "text", "text","text", "text", "text", "text", "text","text", "text", "text", "text", "text","numeric", "text", "text", "text","text", "text", "text", "text", "text","text", "text", "text", "text", "text","text", "text", "text", "text"))
 
+#filter dataset to only include the local authorities we are interested in
 x <- childcare %>%
   filter(`Local Authority`%in% la$`Local authority`)
 
@@ -54,7 +57,7 @@ view(counts)
 countsla <- table(as1$`Authority code`, useNA ="ifany")
 view(countsla)
 
-# introduce the Authority codes
+# assigning the correct Authority codes to the correct local authorities
 as1$`Authority code`[as1$`Authority code` %in% "Bolton"] <- "E08000001"
 as1$`Authority code`[as1$`Authority code` %in% "Bury"] <- "E08000002"
 as1$`Authority code`[as1$`Authority code` %in% "Manchester"] <- "E08000003"
@@ -65,23 +68,6 @@ as1$`Authority code`[as1$`Authority code` %in% "Stockport"] <- "E08000007"
 as1$`Authority code`[as1$`Authority code` %in% "Tameside"] <- "E08000008"
 as1$`Authority code`[as1$`Authority code` %in% "Trafford"] <- "E08000009"
 as1$`Authority code`[as1$`Authority code` %in% "Wigan"] <- "E08000010"
-
-counts <- table(as1$`Local Authority`, useNA ="ifany")
-view(counts)
-countsla <- table(as1$`Authority code`, useNA ="ifany")
-view(countsla)
-
-# E08000001	Bolton
-# E08000002	Bury
-# E08000003	Manchester
-# E08000004	Oldham
-# E08000005	Rochdale
-# E08000006	Salford
-# E08000007	Stockport
-# E08000008	Tameside
-# E08000009	Trafford
-# E08000010	Wigan
-
 
 
 # correct the dates to remove the time 
@@ -101,7 +87,7 @@ view(counts)
 
 # Assign name "NA" to the missing values within the counts object 
 names(counts)[is.na(names(counts))] <- "NA"
-# Display barplot 
+# Display barplot to see nulls 
 barplot(counts, main="Registered places", xlab='Counts', ylab=('Registered places'), horiz=TRUE)
 
 
@@ -115,18 +101,18 @@ test2 <- test
 # check the column names
 names(test2)
 
-# rename some columns
+# rename some columns for consistency
 names(test2)[2] <- "Registration date"
 names(test2)[6] <- "Authority name"
 test2$`Reg date` <- test2$`Registration date`
 test2 <- test2[c(1,8,2,3,4,5,6,7)]
 # view(test2)
 
-
+#Separate the date into day month and year columns
 datesep <- separate(test2,'Registration date', c('reg day','reg month', 'reg year'), sep = '/')
 # view(datesep)
 
-# Add a column for the TimeID
+# join the column for the TimeID from the time dataset
 cleanedcare <- sqldf("SELECT time.TimeID, datesep.`Reg date`, datesep.`Provider URN`, datesep.`Provider type`,
 datesep.`Provider name`, datesep.`Authority code`, datesep.`Authority name`, datesep.`Registered places`
 FROM datesep
@@ -134,7 +120,7 @@ JOIN time
 ON datesep.`reg month`=time.MonthName AND datesep.`reg year`=time.Year")
 # view(cleanedcare)
 
-# Add a column for the ProviderID
+# join the column for the ProviderID from the provider dataset
 cleanedcare <- sqldf("SELECT cleanedcare.TimeID, cleanedcare.`Reg date`, cleanedcare.`Provider URN`, provider.`ProviderID`, cleanedcare.`Provider type`,
 cleanedcare.`Provider name`, cleanedcare.`Authority code`, cleanedcare.`Authority name`, cleanedcare.`Registered places`
 FROM cleanedcare
@@ -154,43 +140,17 @@ cleanedcare$`Authority code` <- as.character(cleanedcare$`Authority code`)
 cleanedcare$`Authority name` <- as.character(cleanedcare$`Authority name`)
 cleanedcare$`Registered places` <- as.numeric(cleanedcare$`Registered places`)
 
+#check data type for specific column
 class(cleanedcare$`Reg date`)
 
+#check data summary
 summary(cleanedcare)
+
+#view cleaned dataset
 view(cleanedcare)
 
-
-# Run the following section by section to view how many nulls are present 
-# Showed no nulls in provider URN
-counts <- table(cleanedcare$`Provider URN`, useNA ="ifany")
-view(counts)
-
-# no nulls here
-counts <- table(cleanedcare$`Reg date`, useNA ="ifany")
-view(counts)
-
-# no nulls in provider type
-counts <- table(cleanedcare$`Provider type`, useNA ="ifany")
-view(counts)
-
-# no nulls in provider name 
-counts <- table(cleanedcare$`Provider name`, useNA ="ifany")
-view(counts)
-
-# no nulls here
-counts <- table(cleanedcare$`Authority code`, useNA ="ifany")
-view(counts)
-
-# no nulls here
-counts <- table(cleanedcare$`Authority name`, useNA ="ifany")
-view(counts)
-
-# no nulls in registered places
-counts <- table(cleanedcare$`Registered places`, useNA ="ifany")
-view(counts)
-
 ############# checks ##################
-complete.cases(cleanedcare)
+# data summary and first few values in each column
 str(cleanedcare)
 # checking Non numeric values
 'isitnumeric?'<- unlist(lapply(cleanedcare, is.numeric))
@@ -198,64 +158,72 @@ view(`isitnumeric?`)
 # checking Non character values
 'isitcharacter?'<- unlist(lapply(cleanedcare, is.character))
 view(`isitcharacter?`)
-#checking for null in the object
+#checking for nulls in the object
 is.null(cleanedcare)
-#checking missing values
+complete.cases(cleanedcare)
+#dataset summary
 summary(cleanedcare)
 ############# checks ##################
 
-# save cleaned dataset as a csv file
+# save cleaned dataset as a csv file in working directory
 write.csv(cleanedcare, "clean/cleanedcare.csv", row.names=F)
 
 ######################################################################################################################################################
 # Import Total live births by month and area of usual residence of Mother, 
 # England and Wales, September 2014 to August 2019
 births1419 <- read_excel("raw data used/live births/Merged births.xlsx")
+
+#view what you imported
 view(births1419)
+
+#create variable to work with
 ex1 <- births1419
 
 # change the format of the names of the columns 
 names(ex1)[3:62] <-  format(as.Date(as.numeric(names(ex1)[3:62]),
                                   origin = "1899-12-30"), "%d/%b/%Y")
 
+#filter dataset to only include the local authorities we are interested in
 ex1 <- ex1 %>%
   filter(`Authority name`%in% la$`Local authority`)
 
+#create column for dates and gather dates into this
 ex2 <- gather (ex1, Date, 'new births', -1,-2)
+
+#Duplicate the date column and call this 'Reg date'
 ex2$`Reg date` <- ex2$Date
 view(ex2)
 
+# separate the date into day month and year columns
 sepn <- separate(ex2,'Date', c('reg day','reg month', 'reg year'), sep = '/')
 view(sepn)
 
-# Add a column for the TimeID
+# Join the column for the TimeID from the time dataset
 cleanbirths <- sqldf("SELECT time.TimeID, sepn.`Reg date`, sepn.`Authority code`, sepn.`Authority name`, sepn.`new births`
 FROM sepn
 JOIN time
 ON sepn.`reg month`=time.MonthName AND sepn.`reg year`=time.Year")
 view(cleanbirths)
 
-
+# make sure the data types are correct. It seems sql changes everything to character
 cleanbirths$TimeID <- as.numeric(cleanbirths$TimeID)
 cleanbirths$`Reg date` <- anydate(cleanbirths$`Reg date`)
 cleanbirths$`Authority code` <- as.character(cleanbirths$`Authority code`)
 cleanbirths$`Authority name` <- as.character(cleanbirths$`Authority name`)
 cleanbirths$`new births`<- as.numeric(cleanbirths$`new births`)
 
+#check data type for specific column
 class(cleanbirths$`Reg date`)
 
+#view summary
 summary(cleanbirths)
+
+#view cleaned dataset
 view(cleanbirths)
 
- 
-counts <- table(ex2$`Authority code`, useNA ="ifany")
-counts <- table(ex2$`Authority name`, useNA ="ifany")
-counts <- table(ex2$`Date`, useNA ="ifany")
-counts <- table(z$`TimeID`, useNA ="ifany")
-view(counts)
 
 ############# checks ##################
-complete.cases(cleanbirths)
+# data summary and first few values in each column
 str(cleanbirths)
 # checking Non numeric values
 'isitnumeric?'<- unlist(lapply(cleanbirths, is.numeric))
@@ -263,12 +231,14 @@ view(`isitnumeric?`)
 # checking Non character values
 'isitcharacter?'<- unlist(lapply(cleanbirths, is.character))
 view(`isitcharacter?`)
-#checking for null in the object
+#checking for nulls in the object
 is.null(cleanbirths)
-#checking missing values
+complete.cases(cleanbirths)
+#dataset summary
 summary(cleanbirths)
 ############# checks ##################
 
+#convert dataset to csv file and save in working directory
 write.csv(cleanbirths, "clean/cleanbirths.csv", row.names=F)
 
 
